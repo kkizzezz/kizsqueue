b47da606425a57f003f660c721762094041f0637
kkizzezz <kkizzezz@gmail.com>
2026-04-15 01:04:46 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(({mode}) => {
   return {
     base: './',
+    plugins: [], 
     server: {
       hmr: process.env.DISABLE_HMR !== 'true',
     },
